82d0fda306de228c539a67c28a19a1fca3347512
unknown <nirolph@697-334-Nogrod.sf.k12.internal>
2014-10-22 09:58:43 -0700

    Added Stars

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] starLord;
 //need to make constants
-void setup()
+public void setup()
 {
 	size(800, 800);
 	starLord = new Particle[2000];
@@ -10,7 +26,7 @@ void setup()
 	}
 	starLord[starLord.length -1] = new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	background(0);
 	for (int i = 0; i < starLord.length; ++i) 
@@ -102,3 +118,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
